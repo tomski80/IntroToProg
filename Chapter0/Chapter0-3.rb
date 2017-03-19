6e9e7f64b87ebd6628b1f205b1fb6761d273eeb5
Tom Straszewski <tomek.straszewski@gmail.com>
2017-03-19 18:46:46 +0000

    Added hash table excersise

---
+++ Chapter0/Chapter0-3.rb
@@ -0,0 +1,10 @@
+#
+# hash tables
+#
+films = {:'Die Hard'=>1988, :'Home Alone'=>1990, :'Moana'=>2016, :'Frozen'=>2013, :'Avengers'=>2012}
+
+puts films[:'Die Hard']
+puts films[:'Home Alone']
+puts films[:'Moana']
+puts films[:'Frozen']
+puts films[:'Avengers']
