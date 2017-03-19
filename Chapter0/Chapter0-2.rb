e6266f75c18a7dfe89f4417d90291d819a5309b8
Tom Straszewski <tomek.straszewski@gmail.com>
2017-03-19 18:05:12 +0000

    Updated four digits program - reformated

--- Chapter0/Chapter0-2.rb
+++ Chapter0/Chapter0-2.rb
@@ -10,13 +10,13 @@ if (num.to_i.to_s != num)
   exit
 end
 
-  num = num.to_i
+num = num.to_i
 
-  if num>=999 && num<=9999
-    puts "Tousends place: #{(num/1000) % 10}"
-    puts "Hundreds place: #{(num/100) % 10} "
-    puts "Tens place: #{(num/10) % 10}"
-    puts "Ones place: #{num % 10}"
-  else
-    puts "That was NOT 4 digit integer!"
-  end
+if num>=999 && num<=9999
+  puts "Tousends place: #{(num/1000) % 10}"
+  puts "Hundreds place: #{(num/100) % 10} "
+  puts "Tens place: #{(num/10) % 10}"
+  puts "Ones place: #{num % 10}"
+else
+  puts "That was NOT 4 digit integer!"
+end
